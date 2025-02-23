b60425b7f1ec0236032ceb25cdfa1df2f72cf2c9
moni-dz <lythe1107@gmail.com>
2025-02-24 04:02:05 +0800

    next auth

---
+++ src/auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import { db } from "./db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    pages: {
+        signIn: "/",
+    },
+    providers: [
+        Credentials({
+            credentials: {
+                username: {},
+                password: {},
+            },
+
+            async authorize(credentials) {
+                const parsed = z
+                    .object({ username: z.string().min(1), password: z.string().min(6) })
+                    .safeParse(credentials);
+
+                if (parsed.success) {
+                    const { username, password } = parsed.data;
+
+                    const user = await db.query.engineer.findFirst({
+                        with: { username, password: Bun.password.hash(password) },
+                    });
+
+                    if (!user) return null;
+                }
+
+                return null;
+            }
+        })
+    ],
+})
