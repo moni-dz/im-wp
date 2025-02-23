5c8127a79b98219fa9c852e8501a1ab48a6adce2
arcadetokyo <pepperpiccio@gmail.com>
2025-02-23 13:13:07 +0800

    update for projects

--- src/components/types.ts
+++ src/components/types.ts
@@ -2,18 +2,19 @@ export interface User {
   username: string;
   isLoggedIn: boolean;
 }
-
-export interface Project {
-  id: number;
-  name: string;
-  description: string;
-  date_added: string;
-}
-
 export interface Employee {
   id: number;
   person_id: number;
   status: string;
   skills: string;
   date_contracted: string;
+}
+export interface Project {
+  id: number;
+  name: string;
+  description: string;
+  status: 'active' | 'completed';
+  startDate: string;
+  endDate?: string;
+  progress: number;
 }
