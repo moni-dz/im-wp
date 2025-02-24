2cc3a49ffe91314580593671ada3f5154be68346
arcadetokyo <pepperpiccio@gmail.com>
2025-02-24 23:46:08 +0800

    added client

--- src/components/types.ts
+++ src/components/types.ts
@@ -2,6 +2,7 @@ export interface User {
   username: string;
   isLoggedIn: boolean;
 }
+
 export interface Employee {
   id: number;
   person_id: number;
@@ -9,6 +10,13 @@ export interface Employee {
   skills: string;
   date_contracted: string;
 }
+
+export interface Client {
+  id: number;
+  name: string;
+  location: string;
+}
+
 export interface Project {
   id: number;
   name: string;
@@ -17,4 +25,6 @@ export interface Project {
   startDate: string;
   endDate?: string;
   progress: number;
+  client_id?: number;
+  client?: Client;
 }
